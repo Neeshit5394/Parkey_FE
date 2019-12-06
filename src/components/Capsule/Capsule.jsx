22a3734fdabec480f7d1f253a06051f7bf0239ad
neeshit <ndangi@stevens.edu>
2019-12-06 17:10:26 -0500

    dashboard creation'

--- src/components/Capsule/Capsule.jsx
+++ src/components/Capsule/Capsule.jsx
@@ -3,6 +3,7 @@ import NavBar from "../NavBar/index";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import Parkings from "../Parkings";
 import LandingPage from "../Landing";
+import ProfileSection from "../ProfileSection";
 import User from "../User";
 import { connect } from "react-redux";
 import { signIn } from "./../../store/actions";
@@ -29,11 +30,12 @@ class Capsule extends Component {
         <div>
           <NavBar />
           <Router>
-            <Switch>
+            {/* <Switch>
               <Route path="/" exact component={LandingPage} />
               <Route path="/user" component={User} />
               <Route path="/Parkings/:id" component={Parkings} />
-            </Switch>
+              <Route path="/ProfileSection" component={ProfileSection}/>
+            </Switch> */}
           </Router>
         </div>
       );
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Styled from "./styled";
+import { connect } from "react-redux";
+
+class Dashboard extends Component {
+  chartRef = React.createRef();
+  state = {
+    hasError: false,
+
+  }
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return (
+      <Styled.container>
+
+      </Styled.container>
+    );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
+  };
+}
+
+export default Dashboard;
